b5f05d37467949c4c1f0c8ecf13d2f8e01209fcb
Anna <annas.wieclaw@gmail.com>
2019-05-22 16:52:12 +0100

    Journey knows its cost

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,8 +2,19 @@ class Journey
   attr_reader :entry_station, :history
   attr_accessor :exit_station
 
+  MINIMUM_FARE = 1
+  PENALTY_FARE = 6
+
   def initialize(entry_station = nil)
     @entry_station = entry_station
     @exit_station = nil
+  end
+
+  def fare
+    valid_journey? ? MINIMUM_FARE : PENALTY_FARE
+  end
+
+  def valid_journey?
+    !(@entry_station && @exit_station).nil?
   end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,19 +1,31 @@
-require './lib/journey'
+require "./lib/journey"
 describe Journey do
+  let(:barbican_station) { double :barbican_station }
+  let(:bow_station) { double :bow_station}
   let(:journey) {
-    Journey.new("Barbican")
+    Journey.new(barbican_station)
   }
-  it 'has an entry_station' do
+  it "has an entry_station" do
     expect(journey).to respond_to(:entry_station)
   end
 
-  it 'knows the name of the entry station' do
-    expect(journey.entry_station).to eq("Barbican")
+  it "knows the name of the entry station" do
+    expect(journey.entry_station).to eq(barbican_station)
   end
 
   it "knows the name of exit station" do
-    journey.exit_station = "Bow"
-    expect(journey.exit_station).to eq("Bow")
+    journey.exit_station = bow_station
+    expect(journey.exit_station).to eq(bow_station)
   end
 
+  describe "knows how much the journey costs" do
+    it "costs minimum fare for valid journey" do 
+      journey.exit_station = bow_station
+      expect(journey.fare).to eq(Journey::MINIMUM_FARE)
+    end
+
+    it "costs penalty fare for invalid journey" do 
+      expect(journey.fare).to eq(Journey::PENALTY_FARE)
+    end
+  end
 end
